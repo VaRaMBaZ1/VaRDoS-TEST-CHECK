a7575855cd44f08bc990b7ba47dfba23d6ab81d3
VaRaMBaZ1 <92698950+VaRaMBaZ1@users.noreply.github.com>
2022-05-08 14:25:38 +0800

    Add files via upload

--- ddos.py
+++ ddos.py
@@ -179,11 +179,10 @@ while True:
         checksite = requests.post(url, headers=header, proxies=proxieshttphttp)
         if checksite.status_code >= 500:
             statustext = "OFF_LINE"
-            clolor = 'colorama.Fore.RED'
+            print(colorama.Fore.RED)
         else:
             statustext = "ON_LINE" 
-            clolor = 'colorama.Fore.GREEN'
+            print(colorama.Fore.GREEN)
     except:
         pass
-    
-    print("\r", clolor, "Check Site | Status: ", checksite.status_code, " | ", statustext, end='')
+    print("\r", "Check Site | Status: ", checksite.status_code, " | ", statustext, end='')
